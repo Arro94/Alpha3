3846916ac402da3fd1c82d0d4c43127d7ee5838f
Arron <christie.86@osu.edu>
2019-02-14 00:54:30 -0500

    testing subscription routing entirely through app.js instead of using exported router from EmailList.js

--- Alphamated/app.js
+++ Alphamated/app.js
@@ -3,10 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const options = require ('./options');
+const router = express.Router();
+const fs = require( 'fs' );
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var emailRouter = require('./routes/EmailList');
+// var emailRouter = require('./routes/EmailList');
 
 var app = express();
 
@@ -22,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/subscribeToEmail', emailRouter);
+// app.use('/subscribeToEmail', emailRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,6 +41,30 @@ app.use(function(err, req, res, next) {
     // render the error page
     res.status(err.status || 500);
     res.render('error');
+});
+
+
+
+
+router.post('/subscribeToEmail', (req, res) => {
+    const name = req.body.txtName;
+    const email = req.body.txtEmail;
+    if(!name || !email) {
+        res.sendStatus(400);
+        console.info('  Bad request - Request does not contain name or email');
+    } else {
+        const path = req.body.filePath ?
+            options.assetsDir + req.body.filePath : options.emailList;
+        if(!fs.existsSync(path)) {
+            fs.writeFileSync(path, 'wx');
+        }
+        const entry = '\n' + name + ' (' + email + ')';
+        fs.appendFileSync(path, entry);
+        console.info('  Added ' + name + ' (' + email + ') ' + ' to the distribution list');
+        res.sendStatus(204);
+    }
+    //res.render('./../public/contact.html', { title: 'Alphamated' });
+
 });
 
 module.exports = app;
