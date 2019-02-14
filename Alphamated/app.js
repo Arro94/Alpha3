1a106658b7221379e4aa218973e9cdfd99189087
Arron <christie.86@osu.edu>
2019-02-14 01:07:52 -0500

    try this i guess. fuck it

--- Alphamated/app.js
+++ Alphamated/app.js
@@ -1,16 +1,12 @@
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const options = require ('./options');
-const router = express.Router();
-const fs = require( 'fs' );
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-// var emailRouter = require('./routes/EmailList');
-
 var app = express();
 
 // view engine setup
@@ -25,26 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-// app.use('/subscribeToEmail', emailRouter);
+const router = express.Router();
+const options = require ('./routes/options');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
-
-
-
+const fs = require( 'fs' );
 
 router.post('/subscribeToEmail', (req, res) => {
     const name = req.body.txtName;
@@ -63,9 +43,29 @@ router.post('/subscribeToEmail', (req, res) => {
         console.info('  Added ' + name + ' (' + email + ') ' + ' to the distribution list');
         res.sendStatus(204);
     }
-    //res.render('./../public/contact.html', { title: 'Alphamated' });
-
 });
 app.use(router);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+    //res.render('error');
+
+  res.json({
+    message: err.message,
+       error: err
+  });
+
+});
 
 module.exports = app;
